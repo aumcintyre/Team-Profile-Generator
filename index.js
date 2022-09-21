f16dda1816d1eac56c33b91e9b92e1222cb43bba
aumcintyre <aumcintyre@gmail.com>
2022-09-21 09:21:47 -0500

    Required all classes and node modules

---
+++ index.js
@@ -0,0 +1,19 @@
+//Link to generate page here
+
+//Employee Classes
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+
+//Node Modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//Team array starts blank, will have values pushed based on user-input
+const teamArray = [];
+
+//Manager prompts begin here
+
+
+
+//Employee prompts begin here
