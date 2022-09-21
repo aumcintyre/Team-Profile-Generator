38aefbf482b9661a7b4101d97c3bc55f259af517
aumcintyre <aumcintyre@gmail.com>
2022-09-21 18:20:04 -0500

    completed new employee prompts, need i + e classes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Engineer = require('./lib/Engineer');
 //Node Modules
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { resolve } = require('path');
 
 //Team array starts blank, will have values pushed based on user-input
 const teamArray = [];
@@ -38,15 +39,15 @@ const addManager = () => {
             message: "Enter the manager's office number."
         },
     ])
-    .then(managerInput => {
-        const { name, id, email, officeNumber} = managerInput;
-        const manager = new Manager (name, id, email, officeNumber);
+        .then(managerInput => {
+            const { name, id, email, officeNumber } = managerInput;
+            const manager = new Manager(name, id, email, officeNumber);
 
-        // Won't work until Manager constructor is built out -- NOW WORKING
-        teamArray.push(manager);
-        console.log(manager);
-        console.log(teamArray);
-    })
+            // Won't work until Manager constructor is built out -- NOW WORKING
+            teamArray.push(manager);
+            console.log(manager);
+            console.log(teamArray);
+        })
 }
 
 
@@ -54,15 +55,69 @@ const addManager = () => {
 
 const newEmployee = () => {
     return inquirer.prompt([
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Select employee role.',
+            choices: ['Intern', 'Engineer'],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter employee name.',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter employee ID.',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter employee email adress.',
+        },
+//This question prompts only if 'Engineer' is selected as the role.
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter employee Github Username.',
+            when: (input) => input.role === "Engineer",
+        },
+//This question prompts only if 'Intern' is slected at the role.
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter intern school name.',
+            when: (input) => input.role === "Intern",
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAdd',
+            message: 'Do you want to add another employee?',
+        },
     ])
+    .then(employeeInput => {
+        let { name, id, email, role, github, school, confirmAdd } = employeeInput;
+        let employee;
+
+//Need to build out the Intern and Engineer classes for this portion
+    
+        if(role === "Intern"){
+            employee = new Intern (name, id, email, school);
+        } else if (role === "Engineer"){
+            employee = new Engineer (name, id, email, github);
+        }
+        teamArray.push(employee);
+
+
+        if(confirmAdd){
+            return newEmployee(teamArray);
+        }else {
+            return teamArray;
+        }
+    })
 }
 
 //Call functions and write to index below
 addManager()
+.then(newEmployee)
